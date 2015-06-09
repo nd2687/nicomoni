5706898783d8ffa94a0cb7e6066c415d344a0eb9
Takashi Kawano <kawano.fanta@gmail.com>
2015-06-09 23:37:17 +0900

    Add markdown comment in room

--- app/assets/javascripts/components/_comment.js.jsx
+++ app/assets/javascripts/components/_comment.js.jsx
@@ -1,11 +1,12 @@
 var Comment = React.createClass({
   render: function() {
     var iconImg = `/images/user_icons/user${this.props.icon_number}.png`;
+    var rawMarkUp = marked(this.props.body.toString(), {sanitize: true});
     return (
       <div>
         <img src={iconImg} alt="IconImage" width="20" height="20"></img>
         { this.props.user } said:
-        <p>{ this.props.body }</p>
+        <p dangerouslySetInnerHTML={{__html: rawMarkUp}} />
       </div>
     );
   }
