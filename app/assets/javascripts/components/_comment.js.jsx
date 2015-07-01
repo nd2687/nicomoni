22d9530e380cc57d3ed5ef650051a0e14ecc8e72
Takashi Kawano <kawano.fanta@gmail.com>
2015-07-01 16:04:43 +0900

    Add icon title

--- app/assets/javascripts/components/_comment.js.jsx
+++ app/assets/javascripts/components/_comment.js.jsx
@@ -2,9 +2,10 @@ var Comment = React.createClass({
   render: function() {
     var iconImg = `/images/user_icons/user${this.props.icon_number}.png`;
     var rawMarkUp = marked(this.props.body.toString(), {sanitize: true});
+    var nickname = this.props.user;
     return (
       <div>
-        <img src={iconImg} alt="IconImage" width="20" height="20"></img>
+        <img src={iconImg} alt="IconImage" width="20" height="20" title={nickname} ></img>
         <p dangerouslySetInnerHTML={{__html: rawMarkUp}} />
       </div>
     );
